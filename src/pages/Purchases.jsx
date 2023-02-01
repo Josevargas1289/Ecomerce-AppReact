d39be2718f8d02ac44dd7440d708e2ca348c3b4a
JOSE VARGAS¡¡ <joseroberto-vargas@hotmail.com>
2023-01-31 19:48:58 -0500

    agregando las compras por user

--- src/pages/Purchases.jsx
+++ src/pages/Purchases.jsx
@@ -1,22 +1,47 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { getPurchasesThunk } from '../store/slices/purchases.slice';
 
 const Purchases = () => {
 
-    const purchases = useSelector((state)=> state.purchases)
+    const purchases = useSelector((state) => state.purchases)
     const dispatch = useDispatch();
 
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(getPurchasesThunk())
 
     }, [])
 
     console.log(purchases);
+
+
     return (
+
         <div className='page-purchases'>
-           
-            <h1>My Purchases</h1>
+            <div className='Title-productsId title-purchases'>
+                <Link className='link-producid' as={Link} to={'/'}>Home</Link>
+                <small className='smproducid'></small>
+                <strong> Purchases</strong>
+            </div>
+            <div>
+                <h4>My Purchases</h4>
+            </div>
+            {
+                purchases.map((purchase) => (
+                    <div className='container-mypurchases'>
+                    <ul key={purchase.id}>
+                        <li><img className='img-purchase' src={purchase.product.images[0].url} alt="" /></li>
+                        <li><strong>{purchase.product.title}</strong></li>
+                        <li><span>{purchase.product.createdAt }</span></li>
+
+                    </ul>
+                    </div>
+                ))
+            }
+
+
+
         </div>
     );
 };
